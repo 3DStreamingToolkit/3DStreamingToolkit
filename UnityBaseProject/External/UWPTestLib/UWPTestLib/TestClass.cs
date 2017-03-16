bebb549b6160f32890afbbec838145b281b554b4
Ritchie Lozada <rlozada@microsoft.com>
2017-03-16 16:08:51 -0700

    Unity Plugin Setup for NET 3.5 Class Library Usage.  Initial Setup for UWP Library Test

---
+++ UnityBaseProject/External/UWPTestLib/UWPTestLib/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPTestLib
+{
+    public class TestClass
+    {
+        public string Echo(string msg)
+        {
+            return "Echo: " + msg;
+        }
+    }
+}
