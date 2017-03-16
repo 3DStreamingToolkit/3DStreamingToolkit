674c2403b3f98ba7acb34a66f5ffafdcfe2d5645
Ritchie Lozada <rlozada@microsoft.com>
2017-03-16 16:28:02 -0700

    Platform Switching Setup for Unity Editor vs. UWP (Windows Store) App

--- UnityBaseProject/Assets/Scripts/LibCall.cs
+++ UnityBaseProject/Assets/Scripts/LibCall.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+#if UNITY_EDITOR
 using NETTestLib;
+#else
+using UWPTestLib;
+#endif 
 
 public class LibCall : MonoBehaviour
 {
