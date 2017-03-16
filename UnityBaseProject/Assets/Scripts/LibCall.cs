0f0f15f2d8378356bb1005925250491a8d62cc65
Ritchie Lozada <rlozada@microsoft.com>
2017-03-16 16:08:51 -0700

    Unity Plugin Setup for NET 3.5 Class Library Usage.  Initial Setup for UWP Library Test

---
+++ UnityBaseProject/Assets/Scripts/LibCall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NETTestLib;
+
+public class LibCall : MonoBehaviour
+{
+    public Text statusText;
+
+    public void Call()
+    {
+        var testClass = new TestClass();
+        statusText.text = testClass.Call("Hello!");
+    }
+}
