00ad95e71776053c0622c695be3337ea4de33fdc
Ritchie Lozada <rlozada@microsoft.com>
2017-03-21 15:46:55 -0700

    Setup NET 3.5 DLL plugin for Unity Generated Windows Store App/Universal 10 App

---
+++ UnityBaseProject/Assets/Scripts/LibCallNet35.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TestLibNet35;
+
+public class LibCallNet35 : MonoBehaviour
+{
+    public Text statusText;
+        
+    public void Net35Call()
+    {
+        var call = new Net35Lib();
+        statusText.text = call.Hello("Confirm NET35");
+    }
+}
