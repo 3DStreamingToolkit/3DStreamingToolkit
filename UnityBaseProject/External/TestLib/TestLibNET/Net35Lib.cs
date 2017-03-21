662828b5083e8071dbc8270b1d60a5fb63f2f14e
Ritchie Lozada <rlozada@microsoft.com>
2017-03-21 15:46:55 -0700

    Setup NET 3.5 DLL plugin for Unity Generated Windows Store App/Universal 10 App

---
+++ UnityBaseProject/External/TestLib/TestLibNET/Net35Lib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestLibNet35
+{
+    public class Net35Lib
+    {
+        public string Hello(string msg)
+        {
+            return "Hello " + msg;
+        }
+    }
+}
