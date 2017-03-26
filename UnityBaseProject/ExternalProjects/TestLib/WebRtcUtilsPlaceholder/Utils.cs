16a9dbd3cc95b615d964eb80410901c4353a93de
Ritchie Lozada <rlozada@microsoft.com>
2017-03-26 13:33:37 -0700

    Unity Editor Placholder DLL Setup

---
+++ UnityBaseProject/ExternalProjects/TestLib/WebRtcUtilsPlaceholder/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebRtcUtils
+{
+    public static class Utils
+    {
+        public static string Version()
+        {
+            return "v1.0-alpha Placeholder";
+        }
+
+        public static string CpuUsage()
+        {
+            return "CpuUsage()";
+        }
+
+        public static string LibTestCall()
+        {            
+            return "LibTestCall()";
+        }
+    }
+}
