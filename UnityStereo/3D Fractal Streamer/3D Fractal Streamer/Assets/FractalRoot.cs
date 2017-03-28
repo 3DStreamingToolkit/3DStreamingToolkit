33aadfcb7b65769be0075ba0368fbe116bd1e380
Rick Barraza <rickbarraza@outlook.com>
2017-03-27 20:35:29 -0700

    Initial Commit of Stereo Projects
    3D Receiver spits a stereo texture between eyes will demonstrating a eye
    locked console for debug info. The Fractal project demonstrates a heavy
    load Unity project that can be cloud rendered

---
+++ UnityStereo/3D Fractal Streamer/3D Fractal Streamer/Assets/FractalRoot.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FractalRoot : MonoBehaviour {
+
+    Fractals rootSettings;
+    VoiceManager voiceManager;
+
+    public int absMinimumDrop = 2;
+    public int absMaximumBoost = 8;
+    public bool isHL = false;
+
+    void Start()
+    {
+        rootSettings = this.GetComponent<Fractals>();
+
+        if ( isHL)
+        {
+            voiceManager = this.GetComponent<VoiceManager>();
+            voiceManager.OnVoiceCommand += VoiceManager_OnVoiceCommand;
+        }
+    }
+
+    private void VoiceManager_OnVoiceCommand(string command)
+    {
+        switch (command )
+        {
+            case "boost":
+                Respawn(1);
+                break;
+            case "drop":
+                Respawn(-1);
+                break;
+        }
+    }
+
+    public void Respawn(int step)
+    {
+        int newMaxDepth = rootSettings.maxDepth + step;
+        newMaxDepth = Mathf.Min(Mathf.Max(newMaxDepth, absMinimumDrop), absMaximumBoost);
+        rootSettings.maxDepth = newMaxDepth;
+        clearChildren();
+        rootSettings.spawnFractals();
+    }
+
+    private void clearChildren()
+    {
+        int childs = transform.childCount;
+        for (int i = childs - 1; i >= 0; i--)
+        {
+            GameObject.Destroy(transform.GetChild(i).gameObject);
+        }
+    }
+
+}
+
+
+
