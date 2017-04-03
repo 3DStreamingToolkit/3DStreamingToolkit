3a1aadbb58575081acdb878de9455a87334088fb
Ritchie Lozada <rlozada@outlook.com>
2017-04-02 22:23:35 -0700

    Clean Unity (x86) Reference Project and WebRTC Utilities Initial Setup For use with Mixed Reality x86 Devices

---
+++ UnityX86/ExternalProjects/WebRtcIntegration/WebRtcIntegration/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using PeerConnectionClient.Signalling;
+using WebRtcWrapper;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebRtcIntegration
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private WebRtcUtils _webRtcUtils;        
+
+        public MainPage()
+        {
+            this.InitializeComponent();            
+            _webRtcUtils = new WebRtcUtils();
+            _webRtcUtils.OnInitialized += _webRtcUtils_OnInitialized;
+            _webRtcUtils.PeerVideo = PeerVideo;
+            _webRtcUtils.SelfVideo = SelfVideo;            
+            _webRtcUtils.Initialize();
+        }
+
+        private void _webRtcUtils_OnInitialized()
+        {
+            // Initialize Setup
+            _webRtcUtils.SelectedVideoCodec = _webRtcUtils.VideoCodecs.FirstOrDefault(x => x.Name.Contains("H264"));
+            _webRtcUtils.IsMicrophoneEnabled = false;
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            _webRtcUtils.ConnectCommand.Execute(PeerTextBox.Text);
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            _webRtcUtils.DisconnectFromServerCommand.Execute(null);        }
+
+        private void ConnectToPeerButton_Click(object sender, RoutedEventArgs e)
+        {
+            _webRtcUtils.SelectedPeer = _webRtcUtils.Peers[0];
+            _webRtcUtils.ConnectToPeerCommand.Execute(null);
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            StatusTextBox.Text = string.Empty;
+        }
+    }
+}
---
+++ UnityX86/ExternalProjects/WebRtcIntegration/WebRtcWrapper/Utilities/ValidableIntegerString.cs
@@ -0,0 +1,93 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+
+namespace PeerConnectionClient.Utilities
+{
+    /// <summary>
+    /// Class to validate that the string member variable can be converted 
+    /// to an integer in range [minValue, maxValue].
+    /// </summary>
+    public class ValidableIntegerString : ValidableBase<string>
+    {
+        // Minimum allowed value for the integer
+        private readonly int _minValue;
+
+        // Maximum allowed value for the integer
+        private readonly int _maxValue;
+
+        /// <summary>
+        /// Default constructor to set minimum and maximum integer default values.
+        /// </summary>
+        public ValidableIntegerString()
+        {
+            _minValue = 0;
+            _maxValue = 100;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="minValue">Minimum allowed value for the integer.</param>
+        /// <param name="maxValue">Maximum allowed value for the integer.</param>
+        public ValidableIntegerString(int minValue = 0, int maxValue = 100)
+        {
+            _minValue = minValue;
+            _maxValue = maxValue;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="defaultValue">Default integer value.</param>
+        /// <param name="minValue">Minimum allowed value for the integer.</param>
+        /// <param name="maxValue">Maximum allowed value for the integer.</param>
+        public ValidableIntegerString(int defaultValue, int minValue = 0, int maxValue = 100)
+            : this(minValue, maxValue)
+        {
+            Value = defaultValue.ToString();
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="defaultValue">Default integer value.</param>
+        public ValidableIntegerString(string defaultValue)
+        {
+            Value = defaultValue;
+        }
+
+        /// <summary>
+        /// Validates that the string value can be converted to an integer
+        /// and the intereger will be in range [minValue, maxValue].
+        /// </summary>
+        override protected void Validate()
+        {
+            try
+            {
+                var intVal = Convert.ToInt32(Value);
+                if (intVal >= _minValue && intVal <= _maxValue)
+                {
+                    Valid = true;
+                }
+                else
+                {
+                    Valid = false;
+                }
+            }
+            catch (Exception)
+            {
+                Valid = false;
+            }
+        }
+    }
+}
