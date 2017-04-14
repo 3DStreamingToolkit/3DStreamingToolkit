e2b64ee1a2f1dbd8650604f63a823bf16bc7a621
Tyler Gibson <tgibson@microsoft.com>
2017-04-14 09:06:08 +0000

    M58 Synced lib generator

--- WebRTCLibs/webrtcSetup.ps1
+++ WebRTCLibs/webrtcSetup.ps1
@@ -19,34 +19,81 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 if((Test-Path ($WebRTCFolder)) -eq $false) {
     New-Item -Path $WebRTCFolder -ItemType Directory -Force 
-    $fullPath = (Resolve-Path -Path $WebRTCFolder).path
+}
+$fullPath = (Resolve-Path -Path $WebRTCFolder).path
+Set-Location $fullPath
+if((Test-Path ($fullPath + "\depot_tools\")) -eq $false) {
+    if(-not (Test-Path $PWD\depot_tools.zip)) {
+        Start-BitsTransfer -Source https://storage.googleapis.com/chrome-infra/depot_tools.zip -Destination ((Get-ScriptDirectory) + "\depot_tools.zip")
+    }
+    [System.IO.Compression.ZipFile]::ExtractToDirectory(((Get-ScriptDirectory) + "\depot_tools.zip"), "$fullPath\depot_tools\")
 }
 
-if(-not (Test-Path $PWD\depot_tools.zip)) {
-    Start-BitsTransfer -Source https://storage.googleapis.com/chrome-infra/depot_tools.zip -Destination ((Get-ScriptDirectory) + "\depot_tools.zip")
-}
-[System.IO.Compression.ZipFile]::ExtractToDirectory(((Get-ScriptDirectory) + "\depot_tools.zip"), "$fullPath\depot_tools\")
-
-IF (-not($Env:Path | Select-String -SimpleMatch $fullPath\depot_tools)) { 
+if (-not($Env:Path | Select-String -SimpleMatch $fullPath\depot_tools)) { 
     $Env:Path = "$fullPath\depot_tools;" + $Env:Path
     [Environment]::SetEnvironmentVariable("Path", $Env:Path, [System.EnvironmentVariableTarget]::Machine) 
+    CMD /C "gclient"
 }
-Set-Location $fullPath
-CMD /C "gclient"
+
 $Env:DEPOT_TOOLS_WIN_TOOLCHAIN = 0
 [Environment]::SetEnvironmentVariable("DEPOT_TOOLS_WIN_TOOLCHAIN", $Env:DEPOT_TOOLS_WIN_TOOLCHAIN, [System.EnvironmentVariableTarget]::Machine)
 New-Item -Path . -Name "webrtc-checkout" -ItemType Directory -Force
 Set-Location "webrtc-checkout"
-CMD /C fetch --nohooks webrtc
-CMD /C gclient sync
-Set-Location "src"
-git checkout master
+#if src exists, don't repull, just reset and reapply patch
+if((Test-Path ("src")) -eq $false) {
+    CMD /C "fetch --nohooks webrtc"
+    CMD /C "gclient sync "
+    Set-Location "src"
+    CMD /C "gclient sync --with_branch_heads"
+    CMD /C "git fetch origin"
+} else {
+    Set-Location "src"
+    CMD /C "git reset --hard"
+    CMD /C "git checkout master"
+    CMD /C "git branch -D patch_branch"
+}
+CMD /C "git clean -f"
+CMD /C "git checkout -b patch_branch refs/remotes/branch-heads/58"
+CMD /C "gclient sync --jobs 16"
+CMD /C ("git apply --ignore-whitespace " + $PSScriptRoot + "\nvencoder.patch")
+CMD /C 'git commit -am "nvencoder patch"'
 
-CMD /C 'gn gen out/VsBuild86Release --ide=vs --args="use_rtti=true target_cpu=\"x86\" is_debug=true rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
-CMD /C 'gn gen out/VsBuild64Release --ide=vs --args="use_rtti=true is_debug=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
-CMD /C 'gn gen out/VsBuild64 --ide=vs --args="use_rtti=true rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
-CMD /C 'gn gen out/VsBuild86 --ide=vs --args="use_rtti=true target_cpu=\"x86\" rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
-CMD /C 'ninja -C out/VsBuild86'
-CMD /C 'ninja -C out/VsBuild64'
-CMD /C 'ninja -C out/VsBuild86Release'
-CMD /C 'ninja -C out/VsBuild64Release'
+CMD /C 'gn gen out/Win32/Release --ide=vs --args="use_rtti=true target_cpu=\"x86\" is_debug=true rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
+CMD /C 'gn gen out/Win32/Debug --ide=vs --args="use_rtti=true target_cpu=\"x86\" rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
+CMD /C 'gn gen out/x64/Release --ide=vs --args="use_rtti=true is_debug=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
+CMD /C 'gn gen out/x64/Debug --ide=vs --args="use_rtti=true rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
+CMD /C 'ninja -C out/Win32/Debug'
+CMD /C 'ninja -C out/x64/Debug'
+CMD /C 'ninja -C out/Win32/Release'
+CMD /C 'ninja -C out/x64/Release'
+
+$outPath = New-Item -Path ..\ -Name "dist" -ItemType directory -Force
+Set-Location "webrtc"
+
+Get-ChildItem -Recurse -Filter "*.h" | ForEach-Object {
+    $touchItem = New-Item (Join-Path -Path ($outPath + "\headers\webrtc\") -ChildPath (Resolve-Path -Path $_.FullName -Relative))  -ItemType file -Force
+    Copy-Item $_.FullName -Destination $touchItem.FullName -Force
+}
+Set-Location "..\out\"
+Get-ChildItem -Directory | ForEach-Object {
+    Get-ChildItem -Path $_.FullName -Directory | ForEach-Object {
+         $libPath = New-Item -Path ($outPath.FullName + "\" + (Resolve-Path -Path $_.FullName -Relative)) -ItemType Directory -Force
+
+        Get-ChildItem -Path $_.FullName -Recurse -Filter "*.pdb" | Where-Object {
+            $_.Name -notmatch ".*exe.*" -and $_.Name -notmatch ".*dll.*" } | ForEach-Object {
+            $touchItem = New-Item -Path ($libPath.FullName + "\symbol") -Name $_.Name  -ItemType file -Force
+            Copy-Item $_.FullName -Destination $touchItem.FullName -Force
+        }
+        Get-ChildItem -Path $_.FullName -Recurse -Filter "*.lib" | Where-Object {
+            $_.Name -notmatch ".*exe.*" } | ForEach-Object {
+            $touchItem = New-Item -Path ($libPath.FullName + "\lib") -Name $_.Name  -ItemType file -Force
+            Copy-Item $_.FullName -Destination $touchItem.FullName -Force
+        }
+        Get-ChildItem -Path $_.FullName -Recurse -File | Where-Object {
+            $_.Name -match "\.exe|\.exe\.pdb" } | ForEach-Object {
+            $touchItem = New-Item -Path ($libPath.FullName + "\exe") -Name $_.Name -ItemType file -Force
+            Copy-Item $_.FullName -Destination $touchItem.FullName -Force
+        }
+    }
+}
+Set-Location $PSScriptRoot
