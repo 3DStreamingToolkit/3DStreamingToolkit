d66ec388dac4f17b54f60da522d752e927cc9d6d
Tyler Gibson <tgibson@microsoft.com>
2017-05-02 01:40:37 -0700

    Separating TestRunner and Video_Helper

--- 3dtoolkit-directx/Toolkit3DLibrary/inc/video_helper.h
+++ 3dtoolkit-directx/Toolkit3DLibrary/inc/video_helper.h
@@ -14,99 +14,19 @@
 
 namespace Toolkit3DLibrary
 {
-	template<class T>
-	class CNvQueue 
-	{
-		T** m_pBuffer;
-		unsigned int m_uSize;
-		unsigned int m_uPendingCount;
-		unsigned int m_uAvailableIdx;
-		unsigned int m_uPendingndex;
-
-	public:
-		CNvQueue() : 
-			m_uSize(0),
-			m_uPendingCount(0),
-			m_uAvailableIdx(0),
-			m_uPendingndex(0),
-			m_pBuffer(NULL)
-		{
-		}
-
-		~CNvQueue()
-		{
-			delete[] m_pBuffer;
-		}
-
-		bool Initialize(T* pItems, unsigned int uSize)
-		{
-			m_uSize = uSize;
-			m_uPendingCount = 0;
-			m_uAvailableIdx = 0;
-			m_uPendingndex = 0;
-			m_pBuffer = new T*[m_uSize];
-			for (unsigned int i = 0; i < m_uSize; i++)
-			{
-				m_pBuffer[i] = &pItems[i];
-			}
-
-			return true;
-		}
-
-		T* GetAvailable()
-		{
-			T* pItem = NULL;
-			if (m_uPendingCount == m_uSize)
-			{
-				return NULL;
-			}
-
-			pItem = m_pBuffer[m_uAvailableIdx];
-			m_uAvailableIdx = (m_uAvailableIdx + 1) % m_uSize;
-			m_uPendingCount += 1;
-			return pItem;
-		}
-
-		T* GetPending()
-		{
-			if (m_uPendingCount == 0)
-			{
-				return NULL;
-			}
-
-			T* pItem = m_pBuffer[m_uPendingndex];
-			m_uPendingndex = (m_uPendingndex + 1) % m_uSize;
-			m_uPendingCount -= 1;
-			return pItem;
-		}
-	};
-
-	typedef struct _EncodeFrameConfig
-	{
-		ID3D11Texture2D* pRGBTexture;
-		uint32_t width;
-		uint32_t height;
-	} EncodeFrameConfig;
-
     class VideoHelper
     {
 	public:
 												VideoHelper(ID3D11Device* device, ID3D11DeviceContext* context);
 												~VideoHelper();
 												
-		NVENCSTATUS								Initialize(IDXGISwapChain* swapChain, char* outputFile = nullptr, bool initEncoder = false);
-		NVENCSTATUS								Initialize(IDXGISwapChain* swapChain, EncodeConfig nvEncodeConfig);
-		NVENCSTATUS                             Deinitialize();
-		NVENCSTATUS								SetEncodeProfile(int profileIndex);
-		void									GetDefaultEncodeConfig(EncodeConfig &nvEncodeConfig);
-		void									Capture();
+		NVENCSTATUS								Initialize(IDXGISwapChain* swapChain);
+		NVENCSTATUS                             Deinitialize(); 
 		void									Capture(void** buffer, int* size, int* width, int* height);
 		ID3D11Texture2D*						Capture2DTexture(int* width, int* height);
-		void									GetEncodedFrame(void** buffer, int* size, int* width, int* height);
 		void									GetHeightAndWidth(int* width, int* height);
 
 		// Debug
-		void									PrintConfig(EncodeConfig encodeConfig);
 		ID3D11Device*							m_d3dDevice;
 		ID3D11DeviceContext*					m_d3dContext;
 
@@ -115,19 +35,7 @@ namespace Toolkit3DLibrary
 		IDXGISwapChain*							m_swapChain;
 
 		// NvEncoder
-		CNvHWEncoder*                           m_pNvHWEncoder;
-		uint32_t                                m_uEncodeBufferCount;
-		EncodeOutputBuffer						m_stEOSOutputBfr;
-		EncodeBuffer							m_stEncodeBuffer[MAX_ENCODE_QUEUE];
-		CNvQueue<EncodeBuffer>                  m_EncodeBufferQueue;
-		EncodeConfig							m_encodeConfig;
-		bool									m_encoderInitialized;
 		ID3D11Texture2D*						m_stagingFrameBuffer;
 		D3D11_TEXTURE2D_DESC					m_stagingFrameBufferDesc;
-
-		NVENCSTATUS								InitializeEncoder(DXGI_SWAP_CHAIN_DESC swapChainDesc, EncodeConfig nvEncodeConfig);
-		NVENCSTATUS								AllocateIOBuffers(uint32_t uInputWidth, uint32_t uInputHeight, DXGI_SWAP_CHAIN_DESC swapChainDesc);
-		NVENCSTATUS								ReleaseIOBuffers();
-		NVENCSTATUS                             FlushEncoder();
 	};
 }
--- 3dtoolkit-directx/VideoTestRunner/inc/VideoTestRunner.h
+++ 3dtoolkit-directx/VideoTestRunner/inc/VideoTestRunner.h
@@ -1,18 +1,94 @@
 #pragma once
 
 #include <d3d11.h>
-
-#include "defs.h"
-#include "video_helper.h"
+#include "pch.h"
+#include "nvEncodeAPI.h"
+#include "nvCPUOPSys.h"
+#include "NvHWEncoder.h"
 
 namespace Toolkit3DLibrary
 {
+	template<class T>
+	class CNvQueue
+	{
+		T** m_pBuffer;
+		unsigned int m_uSize;
+		unsigned int m_uPendingCount;
+		unsigned int m_uAvailableIdx;
+		unsigned int m_uPendingndex;
+
+	public:
+		CNvQueue() :
+			m_uSize(0),
+			m_uPendingCount(0),
+			m_uAvailableIdx(0),
+			m_uPendingndex(0),
+			m_pBuffer(NULL)
+		{
+		}
+
+		~CNvQueue()
+		{
+			delete[] m_pBuffer;
+		}
+
+		bool Initialize(T* pItems, unsigned int uSize)
+		{
+			m_uSize = uSize;
+			m_uPendingCount = 0;
+			m_uAvailableIdx = 0;
+			m_uPendingndex = 0;
+			m_pBuffer = new T*[m_uSize];
+			for (unsigned int i = 0; i < m_uSize; i++)
+			{
+				m_pBuffer[i] = &pItems[i];
+			}
+
+			return true;
+		}
+
+		T* GetAvailable()
+		{
+			T* pItem = NULL;
+			if (m_uPendingCount == m_uSize)
+			{
+				return NULL;
+			}
+
+			pItem = m_pBuffer[m_uAvailableIdx];
+			m_uAvailableIdx = (m_uAvailableIdx + 1) % m_uSize;
+			m_uPendingCount += 1;
+			return pItem;
+		}
+
+		T* GetPending()
+		{
+			if (m_uPendingCount == 0)
+			{
+				return NULL;
+			}
+
+			T* pItem = m_pBuffer[m_uPendingndex];
+			m_uPendingndex = (m_uPendingndex + 1) % m_uSize;
+			m_uPendingCount -= 1;
+			return pItem;
+		}
+	};
+
+	typedef struct _EncodeFrameConfig
+	{
+		ID3D11Texture2D* pRGBTexture;
+		uint32_t width;
+		uint32_t height;
+	} EncodeFrameConfig;
+
 	class VideoTestRunner
 	{
 	public:
 		VideoTestRunner(ID3D11Device* device, ID3D11DeviceContext* context);
 		~VideoTestRunner();
 		void									InitializeTest();
+		NVENCSTATUS                             Deinitialize();
 		void									StartTestRunner(IDXGISwapChain* swapChain);
 		void									IncrementTest();
 		void									IncrementTestSuite();
@@ -24,11 +100,16 @@ namespace Toolkit3DLibrary
 		ID3D11Device*							m_d3dDevice;
 		ID3D11DeviceContext*					m_d3dContext;
 		IDXGISwapChain*							m_swapChain;
-		VideoHelper*							m_videoHelper;
 
 		// NvEncoder
 		EncodeConfig							m_encodeConfig;
 		bool									m_encoderInitialized;
+
+		CNvHWEncoder*                           m_pNvHWEncoder;
+		uint32_t                                m_uEncodeBufferCount;
+		EncodeOutputBuffer						m_stEOSOutputBfr;
+		EncodeBuffer							m_stEncodeBuffer[MAX_ENCODE_QUEUE];
+		CNvQueue<EncodeBuffer>                  m_EncodeBufferQueue;
 
 		// TestRunner
 		EncodeConfig							m_minEncodeConfig;
@@ -41,6 +122,14 @@ namespace Toolkit3DLibrary
 		int										m_currentFrame;
 		bool								    m_initialized;
 		char*									m_fileName;
+
+		NVENCSTATUS								InitializeEncoder();
+		NVENCSTATUS								AllocateIOBuffers();
+		NVENCSTATUS								ReleaseIOBuffers();
+		NVENCSTATUS                             FlushEncoder();
+		void									GetDefaultEncodeConfig();
+		NVENCSTATUS								SetEncodeProfile(int profileIndex);
+		void									Capture();
 
 	};
 }
