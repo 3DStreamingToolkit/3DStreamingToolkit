b3c5869a2e3a4d3adb1d99f51142410d9e88f424
Tyler Gibson <tgibson@microsoft.com>
2017-05-04 05:37:15 -0700

    Fixes from bad merge.

--- 3dtoolkit-directx/Samples/MultithreadedRendering/MultithreadedRendering11.cpp
+++ 3dtoolkit-directx/Samples/MultithreadedRendering/MultithreadedRendering11.cpp
@@ -501,14 +501,12 @@ int InitWebRTC(char* server, int port)
 
 #ifdef SERVER_APP
 #ifdef NO_UI
-	DefaultMainWindow wnd(server, port, true, true, true, true);
+	DefaultMainWindow wnd(server, port, FLAG_autoconnect, FLAG_autocall, true, true);
 #else // NO_UI
-	DefaultMainWindow wnd(server, port, true, true,
-		true, false, 1280, 720);
+	DefaultMainWindow wnd(server, port, FLAG_autoconnect, FLAG_autocall, true, false, 1280, 720);
 #endif // NO_UI
 #else // SERVER_APP
-	DefaultMainWindow wnd(server, port, true, true,
-		false, 1280, 720);
+	DefaultMainWindow wnd(server, port, FLAG_autoconnect, FLAG_autocall, false, false, 1280, 720);
 #endif // SERVER_APP
 
 	if (!wnd.Create())
