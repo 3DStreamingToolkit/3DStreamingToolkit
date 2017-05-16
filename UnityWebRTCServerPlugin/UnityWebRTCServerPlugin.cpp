aa4baab6b9a5144d822039b27b11e740992573f8
claurendine <chase@nerduino.com>
2017-05-16 12:04:51 -0500

    Added UnityWebRTCServerPlugin project
    This plugin associates Unity's output frame buffer with the Toolkit3D and provides callbacks to Unity for messages sent through the data channel.  
    
    Note that the debugger will attempt to load Unity development environment and paths to the installed version of Unity may need to be updated.  Also, be sure to place the nvEncConfig and webrtcConfig json files in the editor's executable directory.

---
+++ UnityWebRTCServerPlugin/UnityWebRTCServerPlugin.cpp
@@ -0,0 +1,309 @@
+#define SUPPORT_D3D11 1
+#define WEBRTC_WIN 1
+
+#include <iostream>
+#include <thread>
+#include <string>
+#include <cstdint>
+#include <wrl.h>
+#include <d3d11_2.h>
+#include "IUnityGraphicsD3D11.h"
+#include "IUnityGraphics.h"
+#include "IUnityInterface.h"
+
+#include "video_helper.h"
+#include "conductor.h"
+#include "default_main_window.h"
+#include "flagdefs.h"
+#include "peer_connection_client.h"
+#include "webrtc/base/checks.h"
+#include "webrtc/base/ssladapter.h"
+#include "webrtc/base/win32socketinit.h"
+#include "webrtc/base/win32socketserver.h"
+
+#pragma warning( disable : 4100 )
+#pragma comment(lib, "ws2_32.lib") 
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "imm32.lib")
+#pragma comment(lib, "version.lib")
+#pragma comment(lib, "usp10.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "winmm.lib")
+
+#pragma comment(lib, "dmoguids.lib")
+#pragma comment(lib, "wmcodecdspuuid.lib")
+#pragma comment(lib, "secur32.lib")
+#pragma comment(lib, "msdmo.lib")
+#pragma comment(lib, "strmiids.lib")
+
+#pragma comment(lib, "common_video.lib")
+#pragma comment(lib, "webrtc.lib")
+#pragma comment(lib, "boringssl_asm.lib")
+#pragma comment(lib, "field_trial_default.lib")
+#pragma comment(lib, "metrics_default.lib")
+#pragma comment(lib, "protobuf_full.lib")
+
+
+using namespace Microsoft::WRL;
+using namespace Toolkit3DLibrary;
+
+void(__stdcall*s_onInputUpdate)(const char *msg);
+
+DEFINE_GUID(IID_Texture2D, 0x6f15aaf2, 0xd208, 0x4e89, 0x9a, 0xb4, 0x48, 0x95, 0x35, 0xd3, 0x4f, 0x9c);
+
+
+static IUnityInterfaces* s_UnityInterfaces = nullptr;
+static IUnityGraphics* s_Graphics = nullptr;
+static UnityGfxRenderer s_DeviceType = kUnityGfxRendererNull;
+static ComPtr<ID3D11Device> s_Device;
+static ComPtr<ID3D11DeviceContext> s_Context;
+
+static rtc::scoped_refptr<Conductor> s_conductor = nullptr;
+
+VideoHelper*				g_videoHelper = nullptr;
+static ID3D11Texture2D*		s_frameBuffer = nullptr;
+static ID3D11Texture2D*		s_frameBufferCopy = nullptr;
+
+
+DefaultMainWindow *wnd;
+std::thread *messageThread;
+
+//std::string s_server = "localhost";
+//uint32_t s_port = 8888;
+
+std::string s_server = "signalingserver.centralus.cloudapp.azure.com";
+uint32_t s_port = 3000;
+
+void FrameUpdate()
+{
+}
+
+// Handles input from client.
+void InputUpdate(const std::string& message)
+{
+	if (s_onInputUpdate)
+	{
+		LOG(INFO) << message;
+
+		(*s_onInputUpdate)(message.c_str());
+	}
+
+	/*
+	Json::Reader reader;
+	Json::Value jmessage;
+	if (!reader.parse(message, jmessage))
+	{
+		return;
+	}
+
+	if (jmessage.isMember("type") &&
+		!strcmp(jmessage["type"].asCString(), "message") &&
+		jmessage.isMember("camera-transform"))
+	{
+		// Parses the camera transformation data.
+		const char* data = jmessage["camera-transform"].asCString();
+		std::istringstream datastream(data);
+		std::string token;
+		getline(datastream, token, ',');
+		float eyeX = stof(token);
+		getline(datastream, token, ',');
+		float eyeY = stof(token);
+		getline(datastream, token, ',');
+		float eyeZ = stof(token);
+		getline(datastream, token, ',');
+		float focusX = stof(token);
+		getline(datastream, token, ',');
+		float focusY = stof(token);
+		getline(datastream, token, ',');
+		float focusZ = stof(token);
+
+		// Initializes the eye position vector.
+		const XMVECTORF32 eye = { eyeX, eyeY, eyeZ, 0.f };
+		const XMVECTORF32 lookAt = { focusX, focusY, focusZ, 0.f };
+
+		// Updates the camera view.
+		g_Camera.SetViewParams(
+			eye,
+			lookAt);
+
+		g_Camera.FrameMove(0);
+	}
+	*/
+}
+
+
+void InitWebRTC()
+{
+	rtc::EnsureWinsockInit();
+	rtc::Win32Thread w32_thread;
+	rtc::ThreadManager::Instance()->SetCurrentThread(&w32_thread);
+	rtc::InitializeSSL();
+
+	PeerConnectionClient client;
+
+	wnd = new DefaultMainWindow(FLAG_server, FLAG_port, FLAG_autoconnect, FLAG_autocall,
+		true, 1280, 720);
+	
+	wnd->Create();
+
+	s_conductor = new rtc::RefCountedObject<Conductor>(&client, wnd, &FrameUpdate, &InputUpdate, g_videoHelper);
+	
+	if (s_conductor != nullptr)
+	{
+		MainWindowCallback *callback = s_conductor;
+
+		callback->StartLogin(s_server, s_port);
+		
+		//s_conductor->Login(s_server, s_port);
+	}
+	//s_conductor->Login(s_server, s_port);
+
+	// Main loop.
+	MSG msg;
+	BOOL gm;
+	while ((gm = ::GetMessage(&msg, NULL, 0, 0)) != 0 && gm != -1)
+	{
+		if (!wnd->PreTranslateMessage(&msg))
+		{
+			try
+			{
+				::TranslateMessage(&msg);
+				::DispatchMessage(&msg);
+			}
+			catch (const std::exception& e) { // reference to the base of a polymorphic object
+				std::cout << e.what(); // information from length_error printed
+			}
+		}
+	}
+}
+
+
+static void UNITY_INTERFACE_API OnEncode(int eventID)
+{
+	if (s_Context)
+    {
+		if (s_frameBuffer == nullptr)
+		{
+			ID3D11RenderTargetView* rtv(nullptr);
+			ID3D11DepthStencilView* depthStencilView(nullptr);
+
+			s_Context->OMGetRenderTargets(1, &rtv, &depthStencilView);
+			
+			if (rtv)
+			{
+				rtv->GetResource(reinterpret_cast<ID3D11Resource**>(&s_frameBuffer));
+				rtv->Release();
+
+				D3D11_TEXTURE2D_DESC desc;
+
+				s_frameBuffer->GetDesc(&desc);
+					
+				g_videoHelper->Initialize(s_frameBuffer, desc.Format, desc.Width, desc.Height);
+				//g_videoHelper->LockBuffer();
+				
+				s_frameBuffer->Release();
+				
+				messageThread = new std::thread(InitWebRTC);
+			}
+		}
+
+		/*
+		LOG(INFO) << "Unlock...";
+		g_videoHelper->UnlockBuffer();
+		
+		Sleep(1);
+		
+		g_videoHelper->LockBuffer();
+		LOG(INFO) << "Lock...";
+		*/
+        return;
+    }
+}
+
+extern "C" void UNITY_INTERFACE_API OnGraphicsDeviceEvent(UnityGfxDeviceEventType eventType)
+{
+    switch (eventType)
+    {
+        case kUnityGfxDeviceEventInitialize:
+        {
+            s_DeviceType = s_Graphics->GetRenderer();
+            s_Device = s_UnityInterfaces->Get<IUnityGraphicsD3D11>()->GetDevice();
+            s_Device->GetImmediateContext(&s_Context);
+			
+			break;
+        }
+
+        case kUnityGfxDeviceEventShutdown:
+        {
+            s_Context.Reset();
+            s_Device.Reset();
+            s_DeviceType = kUnityGfxRendererNull;
+            break;
+        }
+    }
+}
+
+
+
+extern "C" void	UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+{
+	/*
+    AllocConsole();
+    FILE* out(nullptr);
+    freopen_s(&out, "CONOUT$", "w", stdout);
+
+    std::cout << "Console open..." << std::endl;
+	*/
+    s_UnityInterfaces = unityInterfaces;
+    s_Graphics = s_UnityInterfaces->Get<IUnityGraphics>();
+    s_Graphics->RegisterDeviceEventCallback(OnGraphicsDeviceEvent);
+
+    // Run OnGraphicsDeviceEvent(initialize) manually on plugin load
+    OnGraphicsDeviceEvent(kUnityGfxDeviceEventInitialize);
+
+	// Creates and initializes the video helper library.
+	g_videoHelper = new VideoHelper(s_Device.Get(), s_Context.Get());
+}
+
+extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
+{
+    s_Graphics->UnregisterDeviceEventCallback(OnGraphicsDeviceEvent);
+}
+
+extern "C" UnityRenderingEvent UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API GetRenderEventFunc()
+{
+    return OnEncode;
+}
+
+extern "C" __declspec(dllexport) void Login(char *server, int32_t port)
+{
+	s_server = server;
+	s_port = port;
+
+	s_onInputUpdate = nullptr;
+
+	if (s_conductor != nullptr)
+	{
+		MainWindowCallback *callback = s_conductor;
+
+		callback->StartLogin(s_server, s_port);
+	}
+}
+
+extern "C" __declspec(dllexport) void Close()
+{
+	if (s_conductor != nullptr)
+	{
+		s_conductor->Close();
+	}
+}
+
+
+extern "C" __declspec(dllexport) void SetInputDataCallback(void(__stdcall*onInputUpdate)(const char *msg))
+{
+	s_onInputUpdate = onInputUpdate;
+}
+
