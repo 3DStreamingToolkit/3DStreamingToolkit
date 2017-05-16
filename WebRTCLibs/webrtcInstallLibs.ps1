2fb2ccd00729ae35487bf558ac9471057fd76ae3
Tyler Gibson <tgibson@microsoft.com>
2017-05-16 15:47:28 -0700

    Updating Lib installer script for version checking

--- WebRTCLibs/webrtcInstallLibs.ps1
+++ WebRTCLibs/webrtcInstallLibs.ps1
@@ -1,36 +1,64 @@
 Import-Module BitsTransfer
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path $Invocation.MyCommand.Path
+}
+
 function DecompressZip {
     param( [string] $filename, [string] $blobUri = "https://3dtoolkitstorage.blob.core.windows.net/libs/" )
     
-    if((Test-Path ($PSScriptRoot + "\" + $filename)) -eq $false) {
-        Start-BitsTransfer -Source ($blobUri + $filename) -Destination ($PSScriptRoot + "\" + $filename)   
+    $uri = ($blobUri + $filename + ".zip")
+    $request = [System.Net.HttpWebRequest]::Create($uri)
+    $request.Timeout = 10000
+    $response = $request.GetResponse()
+    $etag = $response.Headers["ETag"] 
+    $request.Abort()
+    $localFileName = ($filename + $etag + ".zip")
+    $localFullPath = ($PSScriptRoot + "\" + $localFileName)
+    
+    Get-ChildItem -File -Path $PSScriptRoot -Filter ("*" + $filename + "*") | ForEach-Object { #
+        if($_.Name -notmatch (".*" + $etag + ".*")) {
+                Write-Host "Removing outdated lib"
+                Remove-Item * -Include $_.Name
+        }
+    }
+
+
+
+    if((Test-Path ($localFullPath)) -eq $false) {
+        Start-BitsTransfer -Source $uri -Destination $localFullPath    
         Write-Host ("Downloaded " + $filename + " lib archive")
-    } 
 
-    $extractDir = ""
+        $extractDir = ""
 
-    if($filename -like "*x64*") {
-        $extractDir = "\x64"
-    } 
-    if($filename -like "*Win32*") {
-        $extractDir = "\Win32"
-    }
-    if($filename -like "*headers*") {
-        $extractDir = "\headers"
-    }
-    if($extractDir -eq "") {
-        return
-    }
+        if($filename -like "*x64*") {
+            $extractDir = "\x64"
+        } 
+        if($filename -like "*Win32*") {
+            $extractDir = "\Win32"
+        }
+        if($filename -like "*headers*") {
+            $extractDir = "\headers"
+        }
+        if($extractDir -eq "") {
+            return
+        }
+        
+        if((Test-Path ($PSScriptRoot + $extractDir)) -eq $true) {
+            Write-Host "Clearing existing $extractDir" 
+            Remove-Item -Recurse -Force ($PSScriptRoot + $extractDir)
+        }
 
-    if((Test-Path ($PSScriptRoot + $extractDir)) -eq $false) {
         Write-Host "Extracting..."
-        [System.IO.Compression.ZipFile]::ExtractToDirectory($PSScriptRoot + "\" + $filename, $PSScriptRoot)
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($localFullPath, $PSScriptRoot)
         Write-Host "Finished"
     }
 }
 
-DecompressZip -filename "m58patch_x64.zip"
-DecompressZip -filename "m58patch_Win32.zip"
-DecompressZip -filename "m58patch_headers.zip"
+
+DecompressZip -filename "m58patch_headers"
+DecompressZip -filename "m58patch_x64"
+DecompressZip -filename "m58patch_Win32"
