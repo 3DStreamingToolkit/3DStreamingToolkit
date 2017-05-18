b9410a2f722635ed0339bb6ca1e8858701777bf3
Tyler Gibson <tgibson@microsoft.com>
2017-05-18 11:17:47 -0700

    Updating CI Script for webrtc libraries

--- WebRTCLibs/webrtcSetupTestCI.ps1
+++ WebRTCLibs/webrtcSetupTestCI.ps1
@@ -59,6 +59,18 @@ CMD /C "gclient sync --jobs 16"
 CMD /C ("git apply --ignore-whitespace " + $PSScriptRoot + "\nvencoder.patch")
 CMD /C 'git commit -am "nvencoder patch"'
 
+CMD /C 'gn gen citest/Win32/Release  --ide=vs --args="use_rtti=true target_cpu=\"x86\" is_debug=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_initialize_ffmpeg=true is_official_build=true"'
+CMD /C 'gn gen citest/Win32/Debug    --ide=vs --args="use_rtti=true target_cpu=\"x86\" is_debug=true  rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_initialize_ffmpeg=true"'
+CMD /C 'gn gen citest/x64/Release    --ide=vs --args="use_rtti=true target_cpu=\"x64\" is_debug=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_initialize_ffmpeg=true is_official_build=true"'
+CMD /C 'gn gen citest/x64/Debug      --ide=vs --args="use_rtti=true target_cpu=\"x64\" is_debug=true  rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_initialize_ffmpeg=true"'
+CMD /C 'ninja -C citest/Win32/Debug'
+CMD /C 'ninja -C citest/x64/Debug'
+CMD /C 'ninja -C citest/Win32/Release'
+CMD /C 'ninja -C citest/x64/Release'
+
+#Run Unit tests suite and validation checks here
+#Continue if successful to build packaged bits
+
 CMD /C 'gn gen out/Win32/Release  --ide=vs --args="use_rtti=true target_cpu=\"x86\" is_debug=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true is_official_build=true"'
 CMD /C 'gn gen out/Win32/Debug    --ide=vs --args="use_rtti=true target_cpu=\"x86\" is_debug=true  rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true"'
 CMD /C 'gn gen out/x64/Release    --ide=vs --args="use_rtti=true target_cpu=\"x64\" is_debug=false rtc_use_h264=true ffmpeg_branding=\"Chrome\" use_openh264=true rtc_include_tests=false libyuv_include_tests=false build_libsrtp_tests=false rtc_initialize_ffmpeg=true is_official_build=true"'
