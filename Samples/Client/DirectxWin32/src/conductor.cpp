afc07ac2930513c21b50a041c5b56bc305b0f164
bengreenier <ben@bengreenier.com>
2017-06-25 18:09:54 -0400

    fix clientName typo

--- Samples/Client/DirectxWin32/src/conductor.cpp
+++ Samples/Client/DirectxWin32/src/conductor.cpp
@@ -497,7 +497,7 @@ void Conductor::StartLogin(const std::string& server, int port)
 	}
 
 	server_ = server;
-	client_->Connect(server, port, "rendering_client_" + GetPeerName());
+	client_->Connect(server, port, "renderingclient_" + GetPeerName());
 }
 
 void Conductor::DisconnectFromServer()
