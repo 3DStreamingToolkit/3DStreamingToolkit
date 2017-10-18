04ed1d55dcbe00e37cd138327d960a8dca7042b0
Andrei Ermilov <anderm@microsoft.com>
2017-10-17 22:38:25 -0400

    Included ThreeDStreamingClient library
    - Added initial flow to connect to a peer on iOS (WIP)
    - Added missing dependencies
    - Fixed peerConnection creation error in iOS ReactNative - frameworks
    were not correctly included in the xcodeproj
    - Linked native libraries in Android build

--- index.ios.js
+++ index.ios.js
@@ -8,29 +8,102 @@ import {
   Picker
 } from 'react-native';
 
-import { ThreeDStreamingClient } from '3dtoolkit';
+var WebRTC = require('react-native-webrtc');
+var {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  getUserMedia,
+} = WebRTC;
+
+import ThreeDStreamingClient from './ThreeDStreamingClient';
 
 import Config from './config';
+
+var streamingClient = new ThreeDStreamingClient({
+  'serverUrl': 'https://signaling-server591a.azurewebsites.net',
+  'peerConnectionConfig': {
+      'iceServers': [{
+          'urls': 'turn:turnserver3dstreaming.centralus.cloudapp.azure.com:5349',
+          'username': 'user',
+          'credential': '3Dtoolkit072017',
+          'credentialType': 'password'
+      }],
+      'iceTransportPolicy': 'relay'
+  }
+}, {
+  RTCPeerConnection: window.mozRTCPeerConnection || window.webkitRTCPeerConnection || RTCPeerConnection,
+  RTCSessionDescription: window.mozRTCSessionDescription || window.RTCSessionDescription || RTCSessionDescription,
+  RTCIceCandidate: window.mozRTCIceCandidate || window.RTCIceCandidate || RTCIceCandidate
+});
 
 export default class rn3dtksample extends Component {
   constructor(props) {
     super(props);
+
+  streamingClient.signIn('ReactClient')
+  .then((this.updatePeerList).bind(this))
+  .then(streamingClient.startHeartbeat.bind(streamingClient))
+  .then(streamingClient.pollSignalingServer.bind(streamingClient, true));
+
+
+  this.state = {otherPeers: streamingClient.otherPeers, 
+                currentPeerId: 0,
+                videoURL: ''};
   }
   componentWillMount() {
 
   }
   componentWillUpdate(props, nextState) {
   }
+
+  updatePeerList() {
+    let peers = streamingClient.otherPeers;
+
+    if(peers.count > 0)
+    {
+      this.setState({currentPeerId: peers[0]});
+    }
+
+    this.setState({otherPeers: streamingClient.otherPeers});
+  }
+
+  onRemoteStreamAdded(event) {
+    this.setState({videoURL: event.stream});
+  }
+
+  onRemoteStreamRemoved(event) {
+  }
+
+  onSessionOpened(event) {
+  }
+
+  onSessionConnecting(event) {
+  }
+
+  joinPeer() {
+    // Join a peer
+    streamingClient.joinPeer(this.state.currentPeerId, {
+      onaddstream: this.onRemoteStreamAdded.bind(this),
+      onremovestream: this.onRemoteStreamRemoved,
+      onopen: this.onSessionOpened,
+      onconnecting: this.onSessionConnecting
+    });
+  }
+
   render() {
     return (
       <View>
-        <Button title='click me' onPress={this.signIn.bind(this)}>Sign In</Button>
         <Picker
           selectedValue={this.state.currentPeerId}
           onValueChange={(itemValue, itemIndex) => this.setState({currentPeerId: itemValue})}>
             {this.renderPeers()}
         </Picker>
-        <Button title="join peer" onPress={this.joinPeer.bind(this)}>Join Peer</Button>
+        <Button title="Join peer" onPress={this.joinPeer.bind(this)}>Join Peer</Button>
+        <RTCView streamURL={this.state.videoURL}/>
       </View>
     );
   }
