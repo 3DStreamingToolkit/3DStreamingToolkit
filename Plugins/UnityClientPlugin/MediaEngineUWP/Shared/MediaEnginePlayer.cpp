cd59c59265cb8e34e4c3d02924337bf1022c5868
Phong Cao <phong.caothai@live.com>
2018-01-04 18:39:21 -0500

    Fixed build server agent issues.

--- Plugins/UnityClientPlugin/MediaEngineUWP/Shared/MediaEnginePlayer.cpp
+++ Plugins/UnityClientPlugin/MediaEngineUWP/Shared/MediaEnginePlayer.cpp
@@ -811,7 +811,7 @@ void MEPlayer::UpdateFrameRate()
 	duration<double, std::milli> time_span = now - m_lastTimeFPSCalculated;
 	if (time_span.count() > 1000) {
 
-		_RPT1(0, "%d\n", _frameCounter);
+		_RPT1(0, "%d\n", m_frameCounter);
 
 		m_renderFps = m_frameCounter;
 		m_frameCounter = 0;
