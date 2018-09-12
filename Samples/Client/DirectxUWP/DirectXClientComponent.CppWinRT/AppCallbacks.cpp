de187ac1755b54831533fcebd4c13a7c15e8c6db
Krzysztof Lesiak <k.lesiak@holo-light.com>
2018-09-12 11:47:41 +0200

    Add most of AppCallbacks and port to standard c++

--- Samples/Client/DirectxUWP/DirectXClientComponent.CppWinRT/AppCallbacks.cpp
+++ Samples/Client/DirectxUWP/DirectXClientComponent.CppWinRT/AppCallbacks.cpp
@@ -1,45 +1,244 @@
 ﻿#include "pch.h"
 #include "AppCallbacks.h"
 
+using namespace ::DirectXClientComponent_CppWinRT;
 using namespace winrt;
-using namespace Windows::ApplicationModel::Core;
-using namespace Windows::UI::Core;
-using namespace Windows::Media::Core;
+using namespace winrt::Windows::ApplicationModel::Core;
+using namespace winrt::Windows::Graphics::Holographic;
+using namespace winrt::Windows::UI::Core;
+using namespace winrt::Windows::Media::Core;
 
 namespace winrt::DirectXClientComponent_CppWinRT::implementation
 {
-	AppCallbacks::AppCallbacks(SendInputDataHandler const& sendInputDataHandler)
+	AppCallbacks::AppCallbacks(SendInputDataHandler const& sendInputDataHandler) :
+		m_videoRenderer(nullptr),
+		m_holographicSpace(nullptr),
+		m_sentStereoMode(false),
+		m_sendInputDataHandler(sendInputDataHandler)
 	{
+	}
+
+	void AppCallbacks::Close()
+	{
+		delete m_videoRenderer;
 	}
 
 	void AppCallbacks::Initialize(CoreApplicationView const& appView)
 	{
+		m_deviceResources = std::make_shared<DX::DeviceResources>();
+		m_main = std::make_unique<HolographicAppMain>(m_deviceResources);
 	}
 
 	void AppCallbacks::SetWindow(CoreWindow const& window)
 	{
+		// Create a holographic space for the core window for the current view.
+		// Presenting holographic frames that are created by this holographic
+		// space will put the app into exclusive mode.
+		m_holographicSpace = HolographicSpace::CreateForCoreWindow(window);
+
+		// The DeviceResources class uses the preferred DXGI adapter ID from
+		// the holographic space (when available) to create a Direct3D device.
+		// The HolographicSpace uses this ID3D11Device to create and manage
+		// device-based resources such as swap chains.
+		m_deviceResources->SetHolographicSpace(m_holographicSpace);
+
+		// The main class uses the holographic space for updates and rendering.
+		m_main->SetHolographicSpace(m_holographicSpace);
 	}
 
 	void AppCallbacks::SetMediaStreamSource(IMediaStreamSource const& mediaSourceHandle, int32_t width, int32_t height)
 	{
+		auto lock = m_lock.Lock();
+		m_mediaSource = mediaSourceHandle.as<ABI::Windows::Media::Core::IMediaStreamSource>();
+
+		if (m_mediaSource != nullptr)
+		{
+			// Initializes the media engine player.
+			m_player = winrt::make_self<CppMEPlayer>(m_deviceResources->GetD3DDevice(), false);
+
+			// Creates a dummy renderer and texture until the first frame is received from WebRTC
+			ID3D11ShaderResourceView* textureView;
+			HRESULT result = m_player->GetPrimaryTexture(
+				width, height, (void**)&textureView);
+
+			// Initializes the video render.
+			m_videoRenderer = new VideoRenderer(m_deviceResources, width, height);
+			m_main->SetVideoRender(m_videoRenderer);
+
+			// Sets the frame transfer callback.
+			m_player->FrameTransferred.add(
+				[&](CppMEPlayer* const mc, int width, int height, Microsoft::WRL::ComPtr<ID3D11Texture2D> texture, int timestampId)
+			{
+				auto lock = m_lock.Lock();
+				if (!m_framePredictionTimestamp.empty())
+				{
+					int64_t predictionTimestamp = m_framePredictionTimestamp[timestampId];
+					std::vector<HolographicFrame>::iterator it = std::find_if(
+						m_holographicFrames.begin(),
+						m_holographicFrames.end(),
+						[&](HolographicFrame const& frame)
+					{
+						//see remarks here for time conversion from cx to cppwinrt: https://docs.microsoft.com/en-us/uwp/api/Windows.Foundation.DateTime
+						return frame.CurrentPrediction().Timestamp().TargetTime().time_since_epoch().count() ==
+							predictionTimestamp;
+					});
+
+					if (it != m_holographicFrames.end())
+					{
+						m_deviceResources->GetD3DDeviceContext()->CopyResource(
+							m_videoRenderer->GetVideoFrame(), texture.Get());
+
+						HolographicFrame frame = *it;
+
+#ifdef SHOW_DEBUG_INFO
+						if (++g_latencyCounter % 60)
+						{
+							g_totalDelayTime += (g_currentTimestamp - predictionTimestamp) / 10000;
+						}
+						else
+						{
+							g_latency = g_totalDelayTime / 60;
+							g_totalDelayTime = 0;
+						}
+
+						if (m_main->Render(frame, m_player->GetFrameRate(), g_latency))
+#else // SHOW_DEBUG_INFO
+						if (m_main->Render(frame))
+#endif // SHOW_DEBUG_INFO
+						{
+							m_deviceResources->Present(frame);
+						}
+
+						m_holographicFrames.erase(it);
+					}
+				}
+			});
+
+			m_player->SetMediaStreamSource(m_mediaSource.get());
+			m_player->Play();
+		}
 	}
 
 	void AppCallbacks::Run()
 	{
+		while (true)
+		{
+			CoreWindow::GetForCurrentThread().Dispatcher().ProcessEvents(
+				CoreProcessEventsOption::ProcessAllIfPresent);
 
+			SendInputData();
+			if (m_player)
+			{
+				m_player->OnVSyncTimer();
+			}
+		}
 	}
 
 	void AppCallbacks::OnPredictionTimestamp(int32_t id, int64_t timestamp)
 	{
+		auto lock = m_lock.Lock();
+		m_framePredictionTimestamp[id] = timestamp;
 	}
 
 	uint32_t AppCallbacks::FpsReport()
 	{
-		return 0;
+		return m_player->GetFrameRate();
 	}
 
 	void AppCallbacks::SendInputData()
 	{
-
+//		// Attempts to set server to stereo mode.
+//		if (!m_sentStereoMode)
+//		{
+//			String^ msg =
+//				"{" +
+//				"  \"type\":\"stereo-rendering\"," +
+//				"  \"body\":\"1\"" +
+//				"}";
+//
+//			if (!m_sendInputDataHandler(msg))
+//			{
+//				return;
+//			}
+//
+//			m_sentStereoMode = true;
+//		}
+//
+//		// Creates a new frame for input data.
+//		HolographicFrame^ newFrame = m_main->Update();
+//		m_holographicFrames.push_back(newFrame);
+//
+//		// Gets the current camera transformation.
+//		XMFLOAT4X4 leftProjectionMatrix;
+//		XMFLOAT4X4 leftViewMatrix;
+//		XMFLOAT4X4 rightProjectionMatrix;
+//		XMFLOAT4X4 rightViewMatrix;
+//		SpatialCoordinateSystem^ currentCoordinateSystem = m_main->GetReferenceFrame()->CoordinateSystem;
+//		for (auto cameraPose : newFrame->CurrentPrediction->CameraPoses)
+//		{
+//			HolographicStereoTransform cameraProjectionTransform =
+//				cameraPose->ProjectionTransform;
+//
+//			Platform::IBox<HolographicStereoTransform>^ viewTransformContainer =
+//				cameraPose->TryGetViewTransform(currentCoordinateSystem);
+//
+//			if (viewTransformContainer != nullptr)
+//			{
+//				HolographicStereoTransform viewCoordinateSystemTransform =
+//					viewTransformContainer->Value;
+//
+//				XMStoreFloat4x4(
+//					&leftProjectionMatrix,
+//					XMMatrixTranspose(XMLoadFloat4x4(&cameraProjectionTransform.Left))
+//				);
+//
+//				XMStoreFloat4x4(
+//					&leftViewMatrix,
+//					XMMatrixTranspose(XMLoadFloat4x4(&viewCoordinateSystemTransform.Left))
+//				);
+//
+//				XMStoreFloat4x4(
+//					&rightProjectionMatrix,
+//					XMMatrixTranspose(XMLoadFloat4x4(&cameraProjectionTransform.Right))
+//				);
+//
+//				XMStoreFloat4x4(
+//					&rightViewMatrix,
+//					XMMatrixTranspose(XMLoadFloat4x4(&viewCoordinateSystemTransform.Right))
+//				);
+//			}
+//		}
+//
+//		// Builds the camera transform message to send.
+//		String^ leftProjection = "";
+//		String^ leftView = "";
+//		String^ rightProjection = "";
+//		String^ rightView = "";
+//		for (int i = 0; i < 4; i++)
+//		{
+//			for (int j = 0; j < 4; j++)
+//			{
+//				leftProjection += leftProjectionMatrix.m[i][j] + ",";
+//				leftView += leftViewMatrix.m[i][j] + ",";
+//				rightProjection += rightProjectionMatrix.m[i][j] + ",";
+//				rightView += rightViewMatrix.m[i][j] + ",";
+//			}
+//		}
+//
+//		String^ cameraTransformBody = leftProjection + leftView + rightProjection + rightView;
+//
+//		// Adds the current prediction timestamp.
+//		cameraTransformBody += newFrame->CurrentPrediction->Timestamp->TargetTime.UniversalTime;
+//		String^ msg =
+//			"{" +
+//			"  \"type\":\"camera-transform-stereo-prediction\"," +
+//			"  \"body\":\"" + cameraTransformBody + "\"" +
+//			"}";
+//
+//#ifdef SHOW_DEBUG_INFO
+//		g_currentTimestamp = newFrame->CurrentPrediction->Timestamp->TargetTime.UniversalTime;
+//#endif // SHOW_DEBUG_INFO
+//
+//		m_sendInputDataHandler(msg);
 	}
 }
--- Samples/Client/DirectxUWP/DirectXClientComponent.CppWinRT/AppCallbacks.h
+++ Samples/Client/DirectxUWP/DirectXClientComponent.CppWinRT/AppCallbacks.h
@@ -5,10 +5,15 @@
 #pragma once
 
 #include "AppCallbacks.g.h"
+#include "Common\DeviceResources.h"
+#include "Content\VideoRenderer.h"
+#include "HolographicAppMain.h"
+#include "MediaEngine\CppMediaEnginePlayer.h"
 #include <cstdint>
 #include <winrt\Windows.ApplicationModel.Core.h>
 #include <winrt\Windows.UI.Core.h>
 #include <winrt\Windows.Media.Core.h>
+#include <wrl\wrappers\corewrappers.h>
 
 namespace winrt::DirectXClientComponent_CppWinRT::implementation
 {
@@ -21,9 +26,26 @@ namespace winrt::DirectXClientComponent_CppWinRT::implementation
 		void SetMediaStreamSource(winrt::Windows::Media::Core::IMediaStreamSource const& mediaSourceHandle, int32_t width, int32_t height);
 		void OnPredictionTimestamp(int32_t id, int64_t timestamp);
 		uint32_t FpsReport();
+		void Close();
 
 	private:
 		void SendInputData();
+
+		Microsoft::WRL::Wrappers::CriticalSection										m_lock;
+		std::shared_ptr<DX::DeviceResources>											m_deviceResources;
+		::DirectXClientComponent_CppWinRT::VideoRenderer*								m_videoRenderer;
+		winrt::com_ptr<CppMEPlayer>														m_player;
+		std::unique_ptr<::DirectXClientComponent_CppWinRT::HolographicAppMain>			m_main;
+		SendInputDataHandler															m_sendInputDataHandler;
+		bool																			m_sentStereoMode;
+		winrt::com_ptr<ABI::Windows::Media::Core::IMediaStreamSource>					m_mediaSource;
+
+		// Frame prediction
+		std::vector<winrt::Windows::Graphics::Holographic::HolographicFrame>			m_holographicFrames;
+		std::map<int, int64_t>															m_framePredictionTimestamp;
+
+		// The holographic space the app will use for rendering.
+		Windows::Graphics::Holographic::HolographicSpace								m_holographicSpace = nullptr;
     };
 }
 
